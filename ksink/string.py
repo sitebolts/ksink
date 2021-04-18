01afa2557daac84fec6d3d2504832f4b046ce1ba
SiteBoltsAdmin <82706374+SiteBoltsAdmin@users.noreply.github.com>
2021-04-18 04:58:45 -0400

    Update string.py

--- ksink/string.py
+++ ksink/string.py
@@ -45,3 +45,44 @@ def get_windows_text_size(text, font_size, font_name):
     ctypes.windll.gdi32.DeleteObject(hfont)
 
     return (size.cx, size.cy)
+
+
+def remove_numbered_words(original_words):
+    if (type(original_words) == list):
+        cleaned_words = [word for word in original_words if bool(regex.search(r'\d', word)) == False]
+        return cleaned_words
+
+    elif (type(original_words) == str):
+        cleaned_words = ' '.join([word for word in original_words.split(' ') if bool(regex.search(r'\d', word)) == False])
+        return cleaned_words
+
+    else:
+        raise Exception('Unknown variable type: Expected original_words to be a str or list, but it was {}'.format(str(type(original_words))))
+
+
+def contains_numbered_words(words):    
+    if (type(words) == list):
+        for word in words:
+            if (bool(regex.search(r'\d', word)) == True):
+                return True
+        return False
+
+    elif (type(words) == str):
+        for word in words.replace('\r\n', ' ').replace('\n', ' ').split(' '):
+            if (bool(regex.search(r'\d', word)) == True):
+                return True
+        return False
+
+    else:
+        raise Exception('Unknown variable type: Expected original_words to be a str or list, but it was {}'.format(str(type(original_words))))
+
+
+#Trims a string until it's no longer than X bytes in UTF8
+#Surely there's a better way to do this?
+def trim_string_to_x_bytes(string, max_bytes, character_encoding):
+    string = string[:max_bytes]
+
+    while (len(string.encode(character_encoding)) > max_bytes):
+        string = string[:-1]
+
+    return string
